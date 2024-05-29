5f16709c1a786d26f24c4452fc03a28a4099c063
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-05-29 20:58:07 -0300

    atualização javascript

--- script/script.js
+++ script/script.js
@@ -1,9 +1,31 @@
 const input = document.getElementById('input');
 
-function buscar(){
-    if(input.value.length == ''){
+function buscar() {
+    if (input.value.length == 0) {
         alert('Por favor, insira um valor!');
-    }else{
-        
+    } else {
+        const ajax = new XMLHttpRequest();
+        const cep = input.value;
+        ajax.open('GET', `https://viacep.com.br/ws/${cep}/json/`);
+        ajax.send();
+        ajax.onload = () => {
+            if (ajax.status == 200) {
+                const response = JSON.parse(ajax.responseText);
+                if (response.erro) {
+                    document.getElementById('area').innerHTML = 'CEP não encontrado!';
+                } else {
+                    document.getElementById('area').innerHTML = `
+                    <p>Logradouro: ${response.logradouro}</p>
+                    <p>Bairro: ${response.bairro}</p>
+                    <p>Cidade: ${response.localidade}</p>
+                    <p>Estado: ${response.uf}</p>
+                    
+
+                    `;
+                }
+            } else {
+                document.getElementById('area').innerHTML = 'Erro ao buscar o CEP!';
+            }
+        }
     }
 }
