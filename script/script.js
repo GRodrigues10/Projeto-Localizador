830ef5e4084d0b9de6c9b5280a3494b2d71d6895
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-05-30 21:02:35 -0300

    update

--- script/script.js
+++ script/script.js
@@ -21,23 +21,21 @@ function buscar() {
                 } else {
 
                     document.getElementById('area').innerHTML = `
-    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
+    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onclick="window.open('https://www.google.com/maps?q=${response.logradouro},${response.bairro},${response.localidade},${response.uf}', '_blank')" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
         <span style="font-weight: bold;">Logradouro:</span> ${response.logradouro}
     </p>
-    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
+    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onclick="window.open('https://www.google.com/maps?q=${response.logradouro},${response.bairro},${response.localidade},${response.uf}', '_blank')" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
         <span style="font-weight: bold;">Bairro:</span> ${response.bairro}
     </p>
-    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
+    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onclick="window.open('https://www.google.com/maps?q=${response.logradouro},${response.bairro},${response.localidade},${response.uf}', '_blank')" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
         <span style="font-weight: bold;">Cidade:</span> ${response.localidade}
     </p>
-    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
+    <p class="cep-info" style="margin: 1px 0; padding: 5px; border: 1px solid #ccc; border-radius: 5px; background-color: rgba(8, 8, 8, 0.62); font-size: 0.9rem; cursor: pointer; transition: background-color 0.3s;" onclick="window.open('https://www.google.com/maps?q=${response.logradouro},${response.bairro},${response.localidade},${response.uf}', '_blank')" onmouseover="this.style.backgroundColor='rgba(8, 8, 8, 0.8)'" onmouseout="this.style.backgroundColor='rgba(8, 8, 8, 0.62)'">
         <span style="font-weight: bold;">Estado:</span> ${response.uf}
     </p>
 `;
                 }
-            } else {
-                document.getElementById('area').innerHTML = 'Erro ao buscar o CEP!';
-            }
+            } 
         }
     }
 }
